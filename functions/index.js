ef8538d4ea289a18085f6714d95d58bee7c291e0
vidhisaini <49809642+vidhisaini@users.noreply.github.com>
2019-07-21 01:09:46 +0530

    .

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+const functions = require('firebase-functions');
+const express = require('express');
+var bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.set("view engine", "ejs");
+
+app.get('/',function(req,res){
+	res.render("home");
+})
+app.get('/courses',function(req,res){
+	res.render("courses");
+})
+app.get('/contact',function(req,res){
+	res.render("contact");
+})
+app.get('/faculty',function(req,res){
+	res.render("faculty");
+})
+
+
+exports.app = functions.https.onRequest(app);
